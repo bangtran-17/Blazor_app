8ef2b4300fb8b3f4e56ddff592c550999ddcc149
Kimanhvo278 <kimanhvo2783@gmail.com>
2023-12-19 22:11:15 +0700

    Thêm giao diện Đánh giá (pháo hoa đùng đùng)

---
+++ Đánh giá/script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('#button');
+const canvas = document.querySelector('#confetti');
+
+const jsConfetti = new JSConfetti();
+
+button.addEventListener('click', () => {
+    jsConfetti.addConfetti({
+        emojis: ['❤️', '⚡️', '💥', '✨', '💫', '🌸','🎄','🦌'],
+    }).then(() => jsConfetti.addConfetti())
+})
