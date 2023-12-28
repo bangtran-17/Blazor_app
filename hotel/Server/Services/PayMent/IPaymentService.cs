406312ec5dbb139f48500f2913d5a55e213df9b4
rauluoc <21521086@gm.uit.edu.vn>
2023-12-28 12:58:54 +0700

    FIx bug

--- hotel/Server/Services/PayMent/IPaymentService.cs
+++ hotel/Server/Services/PayMent/IPaymentService.cs
@@ -6,7 +6,7 @@ namespace Hotel.Server.Services.PayMent
     public interface IPaymentService
     {
         Task<Booking?> PaymentSuccessful(int id);
-        Task<List<Payment?>> GetPayments();
+        Task<List<Payment>> GetPayments();
         Task<Payment?> CreatePayment(Payment Payment);
         Task<Payment?> UpdatePayment(int id, Payment payment);
         Task<Payment?> GetCostById(int id);
